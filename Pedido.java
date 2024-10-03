196fd42d375281f6d0c0937b48b868af970bd695
Lucas <166824755+lucassantx@users.noreply.github.com>
2024-10-03 07:59:38 -0300

    classe Pedido

---
+++ Pedido.java
@@ -0,0 +1,60 @@
+public class Pedido implements IPedido {
+    private String codigoPedido;
+    private Cliente cliente;
+    private Motorista motorista;
+    private Endereco enderecoInicial;
+    private Endereco enderecoFinal;
+
+    public Pedido(String codigoPedido, Cliente cliente, Motorista motorista, Endereco enderecoInicial, Endereco enderecoFinal) {
+        this.codigoPedido = codigoPedido;
+        this.cliente = cliente;
+        this.motorista = motorista;
+        this.enderecoInicial = enderecoInicial;
+        this.enderecoFinal = enderecoFinal;
+    }
+
+    @Override
+    public void fazerPedido() {
+        System.out.println("Pedido realizado com sucesso!");
+    }
+
+    public String getCodigoPedido() {
+        return codigoPedido;
+    }
+
+    public void setCodigoPedido(String codigoPedido) {
+        this.codigoPedido = codigoPedido;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Motorista getMotorista() {
+        return motorista;
+    }
+
+    public void setMotorista(Motorista motorista) {
+        this.motorista = motorista;
+    }
+
+    public Endereco getEnderecoInicial() {
+        return enderecoInicial;
+    }
+
+    public void setEnderecoInicial(Endereco enderecoInicial) {
+        this.enderecoInicial = enderecoInicial;
+    }
+
+    public Endereco getEnderecoFinal() {
+        return enderecoFinal;
+    }
+
+    public void setEnderecoFinal(Endereco enderecoFinal) {
+        this.enderecoFinal = enderecoFinal;
+    }
+}
